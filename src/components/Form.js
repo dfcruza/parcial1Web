48ae5aadab901f93604f032dadb6328d29f5099f
dfcruza <60239198+dfcruza@users.noreply.github.com>
2023-10-05 17:03:18 -0500

    Formulario completado, parcial terminado.

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,81 @@ import { useNavigate } from "react-router-dom";
 
 function Formulario() {
 
+    const navgate = useNavigate();
+
+    const [formValues, setFormValues] = useState({ email: "", password: "" });
+
+    const [validations, setValidations] = useState({ email: true, password: true });
+
+    const handleEmailChange = (event) => {
+        setFormValues({ ...formValues, email: event.target.value });
+    };
+
+    const handlePasswordChange = (event) => {
+        setFormValues({ ...formValues, password: event.target.value });
+    };
+
+    const handleClickSubmit = () => {
+        const credentials = {
+            email: formValues.email,
+            password: formValues.password,
+        };
+
+        fetch("http://localhost:3001/login", {
+            method: "POST",
+            body: JSON.stringify(credentials),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.status === "success") {
+                    setValidations({ ...validations, email: true, password: true });
+                    navgate("/cafes");
+                } else {
+                    setValidations({ ...validations, email: false, password: false });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    const handleCancelClick = () => {
+        alert("Cancelado");
+        navgate("/");
+    };
+
+    return (
+        <Container>
+            <Card style={{ width: "18rem", marginLeft: "100px", marginTop: "50px" }}>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label><FormattedMessage id="email" /></Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" value={formValues.email} onChange={handleEmailChange} />
+                        {validations.email === false && <Form.Text className="text-muted">
+                            <FormattedMessage id="emailError" />
+                        </Form.Text>}
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label><FormattedMessage id="password" /></Form.Label>
+                        <Form.Control type="password" placeholder="Password" value={formValues.password} onChange={handlePasswordChange} />
+                        {validations.password === false && <Form.Text className="text-muted">
+                            <FormattedMessage id="passwordError" />
+                        </Form.Text>}
+                    </Form.Group>
+                    {!validations.email && !validations.password &&<Form.Text className="text-muted">Correo o contraseña incorrectos.</Form.Text>}
+                    <Container>
+                        <Button variant="primary" type="button" onClick={handleClickSubmit}>
+                            <FormattedMessage id="submit" />
+                        </Button>
+                        <Button variant="danger" type="button" onClick={handleCancelClick}>
+                            <FormattedMessage id="cancelar" />
+                        </Button>
+                    </Container>
+                </Form>
+            </Card>
+        </Container>
+    );
 }
 
 export default Formulario;
